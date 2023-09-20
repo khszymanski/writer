9bea9c329890bf9ead9f0b9e0839c9e3b9721a3c
K. H. Szymański <h.szymanski@khs.company>
2023-09-20 22:18:28 +0200

    dodawanie postow

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,19 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostFormType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class PostsController extends AbstractController
 {
     private $postRepository;
+    private $em;
 
-    public function __construct(PostRepository $postRepository){
+    public function __construct(PostRepository $postRepository, EntityManagerInterface $em)
+    {
         $this->postRepository = $postRepository;
+        $this->em = $em;
     }
 
     #[Route('/posts', name: 'app_posts_index', methods: ['GET'])]
@@ -23,19 +29,52 @@ class PostsController extends AbstractController
     {
         $posts = $this->postRepository->findAll();
 
-       return $this->render('posts/index.html.twig', [
-        'posts' => $posts
-       ]);
+        return $this->render('posts/index.html.twig', [
+            'posts' => $posts
+        ]);
     }
-    
-    #[Route('/posts/create', name:'app_posts_create')]
-    public function create(){
 
-        return $this->render('posts/create.html.twig');
+    #[Route('/posts/create', name: 'app_posts_create')]
+    public function create(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newPost = $form->getData();
+
+            $imagePath = $form->get('imagePath')->getData();
+            if ($imagePath) {
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $newPost->setImagePath('/uploads/' . $newFileName);
+            }
+
+
+            $this->em->persist($newPost);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_posts_index');
+        }
+
+        return $this->render('posts/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     #[Route('/posts/{id}', name: 'app_posts_show', methods: ['GET'])]
-    public function show($id): Response{
+    public function show($id): Response
+    {
 
         $post = $this->postRepository->find($id);
 
@@ -43,5 +82,4 @@ class PostsController extends AbstractController
             'post' => $post
         ]);
     }
-
 }
--- src/Form/PostFormType.php
+++ src/Form/PostFormType.php
@@ -6,15 +6,35 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('imagePath')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Tytuł'
+                ],
+                'label' => false
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Opis'
+                ],
+                'label' => false
+            ])
+            ->add('imagePath', FileType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => false
+            ])
         ;
     }
 
